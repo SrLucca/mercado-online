15236b406fcb22e397544fdf57cd3837c1563138
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-07-27 15:08:16 -0300

    adicao do novo campo de imagens no formulario

--- mercado/produto/views.py
+++ mercado/produto/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import QueryDict
-from .forms import Anunciar
+from .forms import Anunciar, ImagensAnuncio
 from usuario.models import Profile
-from produto.models import AnunciarProduto
+from produto.models import AnunciarProduto, ImagemProduto
 from django.contrib import messages
 # Create your views here.
 
 def userAnuncio(request):
     usuario = Profile.objects.filter(user=request.user)
     if request.method == 'POST':
-        form = Anunciar(request.POST)
+        form1 = Anunciar(request.POST)
+        form2 = ImagensAnuncio(request.FILES)
         nome_cadastro = QueryDict.get(request.POST, key='nome')
         descricao_cadastro = QueryDict.get(request.POST, key='descricao')
         categoria_cadastro = QueryDict.get(request.POST, key='categoria')
-        imagens_cadastro = QueryDict.getlist(request.FILES, key='image')
+        imagens_cadastro = QueryDict.get(request.FILES, key='image')
         print(imagens_cadastro)
 
-        if form.is_valid():
-            for img in imagens_cadastro:
-                anuncio = AnunciarProduto.objects.create(nome=nome_cadastro, descricao=descricao_cadastro, categoria=img)
+        if form1.is_valid() and form2.is_valid():
+            anuncio = AnunciarProduto.objects.create(nome=nome_cadastro, descricao=descricao_cadastro, categoria=categoria_cadastro, image=imagens_cadastro)
             anuncio.save()
+            imagens = ImagemProduto.objects.create(produto=anuncio, images=form2)
             for objetos in usuario:
                 objetos.produto_anunciado.add(anuncio)
                 objetos.save()
@@ -27,6 +28,14 @@ def userAnuncio(request):
             return redirect(f"/profile/{request.user.username}")
         else:
             messages.error(request, "Erro no cadastro. Informação Inválida")
-    form = Anunciar()
+    form1 = Anunciar()
+    form2 = ImagensAnuncio()
 
-    return render(request, 'anunciar/cadastro.html', {"form":form})
+    return render(request, 'anunciar/cadastro.html', {"form1":form1,"form2":form2})
+
+def produtoView(request, id):
+
+    produto = get_object_or_404(AnunciarProduto, pk=id)
+    imagens = ImagemProduto.objects.filter(produto=produto)
+
+    return render(request, 'produto_templates/produto.html', {'produto_objs':produto, 'imagem_produto':imagens})
