fe721df3fd57cc21817d70e3960aa42dda980f7b
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-07-27 15:06:16 -0300

    adicao do campo dinamico
    campos de imagens podem ser adicionados ou removidos no cadastro do produto

--- mercado/produto/admin.py
+++ mercado/produto/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from produto.models import ProdutoComprado, AnunciarProduto
+from produto.models import ProdutoComprado, AnunciarProduto, ImagemProduto
 
 # Register your models here.
 
-admin.site.register(AnunciarProduto)
+class ImagemProdutoAdmin(admin.StackedInline):
+    model=ImagemProduto
+
+@admin.register(AnunciarProduto)
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [ImagemProdutoAdmin]
+
+    class Meta:
+        model = AnunciarProduto
+
+
+@admin.register(ImagemProduto)
+class ProdutoImagemAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(ProdutoComprado)
