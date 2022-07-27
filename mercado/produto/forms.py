f0fe46f8fb4f5f3744f9ce433cf3bb8167250a0e
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-07-27 15:06:43 -0300

    [EM ANDAMENTO]
    adicao do campo dinamico de adicao de imagens no formulario de cadastro de produto

--- mercado/produto/forms.py
+++ mercado/produto/forms.py
@@ -1,8 +1,16 @@
 from django import forms
-from .models import AnunciarProduto
+from .models import AnunciarProduto, ImagemProduto
 
 class Anunciar(forms.ModelForm):
 
+
     class Meta:
         model = AnunciarProduto
-        fields = ['nome','descricao','categoria']
+        fields = ['nome','descricao','categoria', 'image']
+
+
+class ImagensAnuncio(forms.ModelForm):
+    
+    class Meta:
+        model = ImagemProduto
+        fields = ['images']
